e36ae21b194f9cc4a0e810f3ab16432045b77003
Johan Anderson <johan@obsolete.se>
2016-02-08 14:57:07 +0100

    Made assignment 4b.

--- Assignment_4B/src/se/mah/kd405a_johan_a/assignment4b/AnimalTest.java
+++ Assignment_4B/src/se/mah/kd405a_johan_a/assignment4b/AnimalTest.java
@@ -37,7 +37,7 @@ public class AnimalTest extends JFrame {
 	 */
 	public AnimalTest() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 696, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new TitledBorder(null, "Animallist", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		contentPane.setLayout(new BorderLayout(0, 0));
